ebed8ade4a224567c234f0e3c016798feec74ead
joo <joo@stashaway.com>
2017-04-28 17:55:34 +0800

    initial setup

--- akka-test/src/main/scala/lejoow/interview/akka/AkkaTestApp.scala
+++ akka-test/src/main/scala/lejoow/interview/akka/AkkaTestApp.scala
@@ -11,17 +11,9 @@ import akka.util.Timeout
   */
 object Main extends App{
 
-  val system = ActorSystem("HelloSystem")
-  val helloActor = system.actorOf(Props[HelloActor], name = "helloActor")
-  helloActor ! "hello"
-  helloActor ! "bye"
-
 
 }
 
 class HelloActor extends Actor{
-  override def receive: Receive = {
-    case "hello" => println("hello back at you")
-    case _ => println("huh?")
-  }
+
 }
--- scala-test/src/main/scala/lejoow/interview/scala/ScalaTestApp.scala
+++ scala-test/src/main/scala/lejoow/interview/scala/ScalaTestApp.scala
@@ -21,13 +21,12 @@ object ScalaTestApp extends App{
   val nums = List(2, -4, 5, 7, 1)
 
   //3-a) from the list, find the elements that are larger than 0 using 'filter'
-  val lessThanZeros = nums.filter(_ > 0)
-
+  val lessThanZeros = nums.filter(_ < 0)
   //3-b) partition the list into two lists, one with numbers larger than zero, and the other with numbers larger than or equal to 0
-  val partitioned = nums.partition(_ >= 0)
+  val partitioned = nums.partition(x => x > 0)
 
   //3-c) drop evey items that are larger than 0
-  val dropped = nums.dropWhile(_ > 0)
+  val dropped = nums.dropWhile(x => x < 0)
 
   println(lessThanZeros)
   println(partitioned)
@@ -36,17 +35,15 @@ object ScalaTestApp extends App{
 
   //4. Complete the factorial function
   def factorial(n: Int): Int =
-    if (n > 1) factorial(n - 1) * n else n
+    if (n == 0) 1 else n * factorial(n - 1)
 
   print(factorial(3))
 
   //5. Complete the above factorial using tailrec
   def factorialTailRec(n: Int): Int = {
-    def loop(acc: Int, n: Int): Int = {
-      if (n == 1) acc else loop(acc * n, n - 1)
+    def calculate(x: Int, acc: Int): Int = {
+      if (x == 0) 1 else calculate(x-1, acc + x)
     }
-
-    loop(n - 1, n)
   }
 
   println(factorial(4) == 24)
@@ -62,5 +59,8 @@ object ScalaTestApp extends App{
 
   case class Sum(e1: Expression, e2: Expression) extends Expression
 
-  def eval(e: Expression): Int = ???
+  def eval(e: Expression): Int = e match {
+    case Number(n) => n
+    case Sum(e1: Exception, e2: Expression) => eval(e1) + eval(e2)
+  }
 }
